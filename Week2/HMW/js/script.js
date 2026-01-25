2febdace546819124c3ab12ad2df1ab932252c34
John S Thanyanop <156885586+SThanyanop@users.noreply.github.com>
2026-01-25 19:02:17 +0700

    finish

--- Week2/HMW/js/script.js
+++ Week2/HMW/js/script.js
@@ -30,58 +30,28 @@ function show_logo() {
 
 function swapCardtoright() {
     const c1 = document.getElementById('intro');
-    const c2 = document.getElementById('personal');
-    const c3 = document.getElementById('work');
-    const c4 = document.getElementById('goal');
-    const c5 = document.getElementById('contact');
+    const c2 = document.getElementById('contact');
 
     if (c1.classList.contains('show')) {
         c1.classList.remove('show');
         c2.classList.add('show');
     }
-    else if (c2.classList.contains('show')) {
-        c2.classList.remove('show');
-        c3.classList.add('show');
-    }
-    else if (c3.classList.contains('show')) {
-        c3.classList.remove('show');
-        c4.classList.add('show');
-    }
-    else if (c4.classList.contains('show')) {
-        c4.classList.remove('show');
-        c5.classList.add('show');
-    }
     else {
-        c5.classList.remove('show');
+        c2.classList.remove('show');
         c1.classList.add('show');
     }
 }
 
 function swapCardtoleft() {
     const c1 = document.getElementById('intro');
-    const c2 = document.getElementById('personal');
-    const c3 = document.getElementById('work');
-    const c4 = document.getElementById('goal');
-    const c5 = document.getElementById('contact');
+    const c2 = document.getElementById('contact');
 
     if (c1.classList.contains('show')) {
         c1.classList.remove('show');
-        c5.classList.add('show');
-    }
-    else if (c2.classList.contains('show')) {
-        c2.classList.remove('show');
-        c1.classList.add('show');
-    }
-    else if (c3.classList.contains('show')) {
-        c3.classList.remove('show');
         c2.classList.add('show');
     }
-    else if (c4.classList.contains('show')) {
-        c4.classList.remove('show');
-        c3.classList.add('show');
-    }
     else {
-        c5.classList.remove('show');
-        c4.classList.add('show');
+        c2.classList.remove('show');
+        c1.classList.add('show');
     }
 }
